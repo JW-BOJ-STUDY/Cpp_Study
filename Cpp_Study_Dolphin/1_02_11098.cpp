2e5b5023b36082e1d2f17113797e1ad3e615d8d4
iDolphin99 <idolphin.kr@gmail.com>
2021-12-31 23:42:34 +0900

    move to Laptop to PC

---
+++ Cpp_Study_Dolphin/1_02_11098.cpp
@@ -0,0 +1,23 @@
+// 11098, √øΩ√∏¶ µµøÕ¡‡
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, p;
+	unsigned int C; 
+	char name; 
+
+	cout << "";
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> p;
+		for (int j = 0; j < p; j++) {
+			cin >> C, name; 
+		}
+	}
+
+
+	return 0;
+}
